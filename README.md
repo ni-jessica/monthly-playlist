ccd43d88ef019f10e52bc8f751ed116e6997a82e
ni-jessica <jessica_ni@brown.edu>
2023-04-19 08:18:52 -0400

    read me and intiial api scrape

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# monthly-playlist
+I love music and using it as a form of digital scrapbooking. 
+
+This app authenticates with Spotify and compiles user data to get your top 20 most listened to tracks this month, as well as 10 similar songs, and creates a playlist with a total of 30 songs that is automatically added to your profile.
+
+It also reccomends an album, an artist, and a song to check out for your next month of listening.
+
+This was created with React, Javascript/Typescript, HTML, CSS, and the Spotify for Developers API.
--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+import {CLIENT_ID} from './env.tsx'
+
+async function GetMonthly() {
+  
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  const REDIRECT_URI = "http://localhost:3000"
+
+  const [accessToken, setAccessToken] = useState("");
+  const [top20, setTop20] = useState([]);
+
+  async function fetchWebApi(endpoint, method, body) {
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      method,
+      body:JSON.stringify(body)
+    });
+    return await res.json();
+  }
+  
+  async function getTopTracks(){
+    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+    return (await fetchWebApi(
+      'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
+    )).items;
+  }
+  
+  const topTracks = await getTopTracks();
+
+  console.log(
+    topTracks?.map(
+      ({name, artists}) =>
+        `${name} by ${artists.map(artist => artist.name).join(', ')}`
+    )
+  );
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    // get the access token
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      setAccessToken(token)
+
+      window.location.hash = ""
+    }
+
+  }, [])
+
+  const logout = () => {
+    setAccessToken("")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <h1>
           Login with Spotify to create a playlist with your top songs for the month!
+          </h1>
+          {!accessToken ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button onClick={logout}>logout</button>
+        }
         </div>
       </header>
     </div>
   );
 }
 
-export default App;
+export default GetMonthly;
