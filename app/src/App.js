edc5d409aad16eb81c075400d03991dcc57c964c
ni-jessica <jessica_ni@brown.edu>
2023-04-19 07:25:54 -0400

    editing base

--- app/src/App.js
+++ app/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          Login with Spotify to create a playlist with your top songs for the month!
+        </div>
       </header>
     </div>
   );
